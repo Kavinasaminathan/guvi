483a5731a27a2bb249a6a41419f8b415a36d24fb
Kavinasaminathan <125350805+Kavinasaminathan@users.noreply.github.com>
2023-03-28 13:16:40 +0530

    Add files via upload

---
+++ login.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 28, 2023 at 09:45 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `Email` varchar(30) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `Phone number` int(20) NOT NULL,
+  `Password` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`Email`, `Username`, `Phone number`, `Password`) VALUES
+('kavinakavina@gmail.com', 'kavinaS', 98765432, 827);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ task/js/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+	$("#save").click(function(){
+		var email=$("#email").val();
+		var password=$("#password").val();
+		if(email==""){
+			$("#email").css("border","1px solid red");
+			$("#email").focus();
+			return false;
+		}
+		else{
+			$("#email").css("border","1px solid green");
+		}
+		if(password==""){
+			$("#password").css("border","1px solid red");
+			$("#password").focus();
+			return false;
+		}
+		else{
+			$("#password").css("border","1px solid green");
+		}
+		$.ajax({
+			type:"POST",
+			url:"php/login.php",
+			dataType:"json",
+			data:$(".loginform").serialize(),
+			success:function(res){
+				if(res.status=="login"){
+					window.location="profile.html";
+				}
+			}
+
+		})
+	})
+})
